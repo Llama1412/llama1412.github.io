49a45e7a1f593192bcb7ef40de16b87bbdcef107
Llama1412 <31854230+Llama1412@users.noreply.github.com>
2017-09-23 12:10:59 +0100

    php calculator

---
+++ php test.php
@@ -0,0 +1,14 @@
+<html>
+<form name="Calculator" action="calculated.php" method="get">
+  <input type="text" name="n1" id="first" value="0">
+  <input type="text" name="n2" id="second" value="0">
+  <select name="Operation">
+  <option value="add">Add</option>
+  <option value="subtract">Subtract</option>
+  <option value="multiply">Multiply</option>
+  <option value="divide">Divide</option>
+</select>
+<input type="submit">
+</form>
+
+</html>
